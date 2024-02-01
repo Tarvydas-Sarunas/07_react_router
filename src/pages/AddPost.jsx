70bb597542ab20b89db799b81825aa5fa965d7b2
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-01 17:54:56 +0100

    klaidu pransimai

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+const postsUrl = 'https://dummyjson.com/posts/add'
+
 export default function AddPost() {
 
   const [inputsObj, setInputsObj] = useState({
     title: 'Cia yra title',
     author: 'James Band',
     tags: 'blue, yellow, red,green',
-    body: '',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit explicabo quod sapiente aliquam excepturi, perferendis quia exercitationem ipsam ratione nostrum!',
+    userId: 5,
   })
+
+  const [errorMsg, setErrorMsg] = useState('')
+const [okMsg, setOkMsg] = useState('')
 
 function hundleInput(e) {
 const {value, name} = e.target
@@ -46,11 +52,46 @@ const finalObj = {
 delete finalObj.tags
     // console;log final obj
     console.log('finalObj ===', finalObj);
+
+    sendToBackEnd(finalObj)
   }
 
+  async function sendToBackEnd(whatToSend) {
+
+try {
+  const resp = await fetch(postsUrl, {
+  method: 'POST',
+  headers: { 'Content-type': 'application/json' },
+  body: JSON.stringify(whatToSend)
+})
+console.log('resp ===', resp);
+showOk(resp)
+const postResult = await resp.json()
+console.log('postResult ===', postResult);
+showError(postResult)
+
+} catch (error) {
+  console.warn('sendToBackEnd error ===', error);
+}
+  }
+
+  function showError(errorMsg) {
+const msg = errorMsg.message
+setErrorMsg(msg)
+  }
+
+  function showOk(okMsge) {
+if(okMsge.status === 200) {
+  const message = 'Your post was send'
+  return setOkMsg(message)
+}
+setOkMsg(okMsg)
+  }
+  console.log('okMsg ===', okMsg);
   return (
     <div className='container'>
       <h1>Create Post</h1>
+      {errorMsg && <h3 className='error'>{errorMsg}</h3>}
       <Link to={'/'}>Back to Home</Link>
 
      <form onSubmit={handleNewPostFormSubmit} className='add-post-form'>
@@ -70,8 +111,10 @@ delete finalObj.tags
         <span>Body</span>
         <textarea value={inputsObj.body} onChange={hundleInput} name='body' cols="30" rows="10"></textarea>
       </label>
+      {okMsg && <h3 className='success'>{okMsg}</h3>}
       <button className='btn' type='submit'>Create post</button>
      </form>
+
     </div>
   )
 }
